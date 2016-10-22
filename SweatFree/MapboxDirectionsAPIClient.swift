0a797828fcb5bcf6b7e2aea11747cc0c160cd761
bmanjuu <bettyofung@gmail.com>
2016-10-21 20:38:04 -0400

    fixed error in sample code from MapboxDirections by changing options into a computed property

--- SweatFree/MapboxDirectionsAPIClient.swift
+++ SweatFree/MapboxDirectionsAPIClient.swift
@@ -11,10 +11,50 @@ import MapboxDirections
 
 struct MapboxDirectionsAPIClient {
     
-    static func getDirectionsToEvent(userLocation: (latitude: Double, longitude:Double), destination: (latitude: Double, longitude: Double)) {
-        
-        let directionsURLString = "https://api.mapbox.com/directions/v5/mapbox/driving/\(userLocation.latitude)%2C\(userLocation.longitude)%3B\(destination.longitude)%2C\(destination.longitude).json?access_token=\(Secrets.mapboxToken)"
+    static let directions = Directions(accessToken: "\(Secrets.mapboxToken)")
+    
+    static let waypoints = [
+        Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.9131752, longitude: -77.0324047), name: "Mapbox"),
+        Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365), name: "White House"),
+        ]
+    
+    static let setRouteOptions = RouteOptions(waypoints: waypoints, profileIdentifier: MBDirectionsProfileIdentifierAutomobile)
+    static var options : RouteOptions {
+        setRouteOptions.includesSteps = true
+        return setRouteOptions
     }
+    
+    let task = directions.calculateDirections(options) { (waypoints, routes, error) in
+        guard error == nil else {
+            print("Error calculating directions: \(error!)")
+            return
+        }
+        
+        if let route = routes?.first, let leg = route.legs.first {
+            print("Route via \(leg):")
+            
+            let distanceFormatter = LengthFormatter()
+            let formattedDistance = distanceFormatter.string(fromMeters: route.distance)
+            
+            let travelTimeFormatter = DateComponentsFormatter()
+            travelTimeFormatter.unitsStyle = .short
+            let formattedTravelTime = travelTimeFormatter.string(from: route.expectedTravelTime)
+            
+            print("Distance: \(formattedDistance); ETA: \(formattedTravelTime!)")
+            
+            for step in leg.steps {
+                print("\(step.instructions)")
+                let formattedDistance = distanceFormatter.string(fromMeters: step.distance)
+                print("— \(formattedDistance) —")
+            }
+        }
+    }
+
+    
+//    static func getDirectionsToEvent(userLocation: (latitude: Double, longitude:Double), destination: (latitude: Double, longitude: Double)) {
+//        
+//        let directionsURLString = "https://api.mapbox.com/directions/v5/mapbox/driving/\(userLocation.latitude)%2C\(userLocation.longitude)%3B\(destination.longitude)%2C\(destination.longitude).json?access_token=\(Secrets.mapboxToken)"
+//    }
     
     
     
