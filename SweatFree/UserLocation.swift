049164c44ab28c3296c2ff38c5ab6d44edff02d7
bmanjuu <bettyofung@gmail.com>
2016-10-25 20:42:55 -0400

    starting to work on obtaining user location using core location framework

---
+++ SweatFree/UserLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UserLocation.swift
+//  SweatFree
+//
+//  Created by Betty Fung on 10/25/16.
+//  Copyright © 2016 Betty Fung. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class UserLocation: NSObject, CLLocationManagerDelegate {
+    
+}
